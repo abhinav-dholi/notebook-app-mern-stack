daf633b852608c619f2b32ab86ce516fb1d735fb
abhinav-dholi <81688737+abhinav-dholi@users.noreply.github.com>
2024-01-16 09:48:50 -0500

    created cards

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,14 +1,15 @@
 import React, {useContext} from 'react'
 import noteContext from '../context/notes/noteContext';
+import NoteItem from './NoteItem';
 
 const Notes = () => {
-    const context = useContext(noteContext);
+  const context = useContext(noteContext);
   const {notes, setNotes} = context;
   return (
-    <div className="container my-3">
+    <div className="row my-3">
       <h2>Your Notes</h2>
       {notes.map((note)=>{
-        return note.title;
+        return <NoteItem note = {note}/>
       })}
       </div>
   )
